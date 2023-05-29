7344a7cfa98a56f6dc128658b5201dd4a29093f6
Lexeor <lexeor.ross@gmail.com>
2023-05-29 14:45:35 +0400

    deprecated items deleted
    - old Content page
    - old languages index.js

--- src/languages/index.ts
+++
@@ -1,9 +0,0 @@
-import en from "./en.json";
-import ru from "./ru.json";
-
-export const dictionaryList = { en: typeof en, ru: typeof ru };
-
-export const languageOptions: { [key: string]: string } = {
-  en: "English",
-  ru: "Русский",
-};
