489d33ae011411d7f54b4f6cd60a29fffc6df899
Lexeor <lexeor.ross@gmail.com>
2023-06-28 16:42:47 +0400

    print page progressed

--- src/components/PrintPage.tsx
+++ src/components/PrintPage.tsx
@@ -18,6 +18,19 @@ const PrintPage = () => {
     return <a href={link.url}>{link.name}</a>;
   });
 
+  let renderReferences = references.map((ref) => {
+    return (
+      <section className="section-ref">
+        <span>
+          <strong>{ref.name}</strong> from <strong>{ref.company}</strong>
+        </span>
+
+        <span>{ref.position}</span>
+        <span>{ref.email}</span>
+      </section>
+    );
+  });
+
   return (
     <div className="container">
       <div className="info-bar print">
@@ -50,12 +63,37 @@ const PrintPage = () => {
       </div>
       <div className="print-content">
         <h2>Profile</h2>
+        <p>
+          For the last 12 years I have worked in the IT department of the
+          Swedish company Sandvik, where I have gone from a Service Desk
+          specialist to an IT infrastructure manager for Russia and the CIS. In
+          my work, I devoted a lot of time to automating business processes
+          using scripts and web applications, and in the end I realized that
+          these tasks are the most interesting for me. It seems to me that now
+          is a great time to change specialization.
+        </p>
         <h2>Strengths</h2>
+        <h3>Collaborative</h3>
+        <p>
+          12 years of work experience in multicultural cross-functional teams
+          located all around the globe.
+        </p>
+        <h3>Eager to learn new skills</h3>
+        <p>
+          As a person switching his specialty I'm constantly seeking for new
+          ways to grow and expand my skills.
+        </p>
+        <h3>Result-oriented</h3>
+        <p>
+          IT Infrastructure manager position taught me to focus on the result
+          and looking for most efficient ways to achieve it.
+        </p>
         <h2>Projects</h2>
         <h2>Employment History</h2>
         <h2>Education</h2>
         <h2>Courses</h2>
         <h2>References</h2>
+        {renderReferences}
       </div>
     </div>
   );
